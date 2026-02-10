0023d2bd58e8660ea7eebf97e45f2fd64e5e4b73
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-10 21:43:48 +0300

    Refactor Cart component to display cart items with product details

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,61 @@
- 
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from "../components/Title"
+
  
 const Cart = () => {
+
+  const { products, currency, cartItems} = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+
+    const tempData = [];
+
+    for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size:item,
+              quantity:cartItems[items][item]
+            })
+          }
+        }
+    }
+    setCartData(tempData);
+
+  }, [cartItems])
+
   return (
-    <div>Cart</div>
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3 text-center">
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+
+      <div className="">
+        {
+          cartData.map((item,index) => {
+            const productData = products.find((product) => product._id === item._id);
+
+            return (
+              <div className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4" key={index}>
+                <div className="flex items-start gap-6">
+                  <img src={productData.image[0]} className='w-16 sm:w-20' alt="" />
+                  <div className="">
+                    <p className="text-xs sm:text-lg font-medium">{productData.name}</p>
+                    <div className="flex items-center gap-5 mt-2">
+                      <p>{currency}{productData.price}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
