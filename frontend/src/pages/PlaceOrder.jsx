f3c30bf834ae33af9316816633b56e5949c326ab
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-11 18:38:01 +0300

    Add padding and border to PlaceOrder component for improved layout

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,36 @@
+import Title from "../components/Title"
 
 const PlaceOrder = () => {
   return (
-    <div className="flex sm:flex-row flex-col justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh]">
+    <div className="flex sm:flex-row flex-col justify-between px-12 gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={'DELIVERY'} text2={'INFORMATION'} />
+        </div>
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="First name" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Last name" />
+        </div>
 
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Email address" />
+
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Street" />
+
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="City" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="State" />
+        </div>
+
+        <div className="flex gap-3">
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Zipcode" />
+          <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="text" placeholder="Country" />
+        </div>
+
+        <input className="border border-gray-300 rounded py-1.5 px-3.5 w-full" type="number" placeholder="Phone" />
+      </div>
+
+      {/* Right side of the checkout page */}
+      
     </div>
   )
 }
