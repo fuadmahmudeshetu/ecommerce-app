181f0a35797fba6426f17059fdf353597ac4d626
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-20 16:52:09 +0300

    feat: implement user registration with validation and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
+import validator from "validator"
+import bcrypt from "bcryptjs"
+import userModel from "../models/userModel.js";
+import  jwt  from "jsonwebtoken";
 
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 // Route for user login
 const loginUser = async (req, res) => {
@@ -7,12 +14,63 @@ const loginUser = async (req, res) => {
 
 // Route for user register
 const registerUser = async (req, res) => {
-    res.json({message: "Register API Working"})
+    try {
+        const { name, email, password } = req.body;
+
+        // checking user already exist or not
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success: false, 
+                message:"User already exist"
+            });
+        }
+
+        // validating email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: "please enter valid email"
+            });
+        }
+
+        if (password.length < 8) {
+            return res.json({
+                success: false,
+                message: "please enter strong password"
+            });
+        }
+
+        // Hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id);
+
+        res.json({
+            success: true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success:false,
+            message: error.message
+        })
+    }
 }
 
 // Route for admin login
 const adminLogin = async (req, res) => {
-    
+
 }
 
 export { loginUser, registerUser, adminLogin }
