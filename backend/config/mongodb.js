3e23c5491a632159e1571a2c6296dec7f86d2fc9
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-20 05:17:48 +0300

    feat: add mongodb dependency and connect to database in server.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
+
+    mongoose.connection.on('connected', () => {
+        console.log("DB connected")
+    })
     
-    await mongoose.connect(`${process.env.MONGODB_URI}/`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js';
 
 //App config
 const app = express();
 const port = process.env.PORT || 5000;
+connectDb();
 
 //  middlewares
 app.use(express.json());
