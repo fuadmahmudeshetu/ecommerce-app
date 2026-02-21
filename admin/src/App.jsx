738fa3f2702fe269d5ac3409f29b7a80108b5df9
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-21 16:54:10 +0300

    feat: initialize admin panel with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created main application structure with App component
    - Implemented routing with react-router-dom for Add, List, and Orders pages
    - Developed Login component for user authentication
    - Added Navbar and Sidebar components for navigation
    - Integrated Toast notifications for user feedback
    - Included assets such as icons and logos
    - Configured Vite for development and production builds
    - Styled application using Tailwind CSS and custom styles

---
+++ admin/src/App.jsx
@@ -0,0 +1,46 @@
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+import { Routes, Route } from "react-router-dom"
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import { useEffect, useState } from "react"
+import Login from "./components/Login"
+import { ToastContainer } from 'react-toastify';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+const App = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {
+        token === "" ? <Login setToken={setToken} /> :
+          <>
+            <Navbar setToken={setToken} />
+            <hr className="border-gray-300" />
+            <div className="flex w-full">
+              <Sidebar />
+              <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+                <Routes>
+                  <Route path="/add" token={token} element={<Add />} />
+                  <Route path="/list" token={token} element={<List />} />
+                  <Route path="/orders" token={token} element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+      }
+
+    </div>
+  )
+}
+
+export default App
