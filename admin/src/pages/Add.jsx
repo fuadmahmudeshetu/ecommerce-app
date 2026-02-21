f8cfe1a0d923c96386e702426c9430c5f1c528d4
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-02-21 18:15:54 +0300

    feat: enhance Add component with image upload and product name input

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <form>
+      <div className="flex flex-col w-full items-start gap-3">
+        <p>Upload Image</p>
+
+        <div className=" flex gap-2">
+          <label htmlFor="image1">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" name="" id="image1" hidden/>
+          </label>
+          <label htmlFor="image2">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" name="" id="image2" hidden/>
+          </label>
+          <label htmlFor="image3">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" name="" id="image3" hidden/>
+          </label>
+          <label htmlFor="image4">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" name="" id="image4" hidden/>
+          </label>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <p className='m-2'>Product Name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required name="" id="" />
+      </div>
+    </form>
   )
 }
 
